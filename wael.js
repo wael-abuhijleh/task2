646e0d4cd03d1efff8af939b58e4515a725540b9
wael-abuhijleh <92797367+wael-abuhijleh@users.noreply.github.com>
2021-10-19 19:54:44 +0300

    Add files via upload

---
+++ wael.js
@@ -0,0 +1,7 @@
+function generatorcat(){
+    var image=document.createElement('img');
+    var div= document.getElementById('flex-cat-gen');
+    image.scr="https://elzero.org/wp-content/uploads/2019/06/learn-html4.png";
+    div.appendChild(image);
+    
+}
